ed94dbef53eb7c39f119f4f89519cdaa70baf63a
wolfm <wolfmans.empire@gmail.com>
2025-05-12 06:59:59 +0500

    intern_hhtp_dto add equals & ghc override

--- HttpDto/InternHttpDto.cs
+++ HttpDto/InternHttpDto.cs
@@ -6,5 +6,16 @@
         public UserHttpDto User { get; set; }
         public ProjectLowDetailHttpDto Project { get; set; }
         public DirectionLowDetailHttpDto Direction { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj != null && obj is InternHttpDto intern)
+            {
+                return intern.Id == Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode(); 
     }
 }
